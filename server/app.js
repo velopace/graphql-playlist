f65706bed218fb6d62357d71425dd37c00d609cd
velopace <velopace@outlook.com>
2021-10-24 16:57:58 -0500

    connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+require('dotenv').config()
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.once('open', () => {
+  console.log('connected database');
+});
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
