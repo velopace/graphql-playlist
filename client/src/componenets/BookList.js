166074312cf47d8d85c5f4942d436414a3482b54
velopace <velopace@outlook.com>
2021-10-25 01:07:48 -0500

    add book details

--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,14 +1,19 @@
 import { useQuery } from '@apollo/client';
+import { useState } from 'react';
 import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
 
 function BookList() {
+  const [selected, setSelected] = useState(null);
   const { loading, error, data } = useQuery(getBooksQuery);
 
   const displayBooks = () => {
     if (loading) return <p>Loading books...</p>;
     if (error) return <p>Error :(</p>;
   
-    return data.books.map(book => <li key={book.id}>{ book.name }</li>);
+    return data.books.map(book => (
+      <li key={book.id} onClick={ (e) => setSelected(book.id) }>{ book.name }</li>
+    ));
   }
 
   return (
@@ -16,6 +21,7 @@ function BookList() {
       <ul id="book-list">
         { displayBooks() }
       </ul>
+      <BookDetails bookId={selected} />
     </div>
   );
 }
