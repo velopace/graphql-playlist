700b3ceef27f0a04e1fc697ef97435e7899d4686
velopace <velopace@outlook.com>
2021-10-24 22:25:21 -0500

    render booklist

--- client/src/componenets/BookList.js
+++ client/src/componenets/BookList.js
@@ -1,9 +1,32 @@
+import { useQuery, gql } from '@apollo/client';
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
 
 function BookList() {
+  const { loading, error, data } = useQuery(getBooksQuery);
+
+  const displayBooks = () => {
+    if (loading) return <p>Loading books...</p>;
+    if (error) return <p>Error :(</p>;
+  
+    return data.books.map(book => {
+      return(
+        <li key={book.id}>{ book.name }</li>
+      );
+    });
+  }
+
   return (
     <div>
       <ul id="book-list">
-        <li>Book name</li>
+        { displayBooks() }
       </ul>
     </div>
   );
--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const schema = require('./schema/schema');
 require('dotenv').config()
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
+
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.once('open', () => {
   console.log('connected database');
