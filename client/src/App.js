2c1223f5012dcb32070169c7823aad521cb65bbc
velopace <velopace@outlook.com>
2021-10-24 22:04:26 -0500

    setup apollo graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+// components
 import BookList from "./componenets/BookList";
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Velopace's Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Velopace's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
